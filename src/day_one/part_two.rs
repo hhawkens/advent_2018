74fcef275ade47a551f6537e37b5c5a07af3d66d
John <purrlinn@web.de>
2018-12-09 21:11:27 +0100

    optimized 1_2 (500x faster)

--- src/day_one/part_two.rs
+++ src/day_one/part_two.rs
@@ -1,19 +1,17 @@
 use super::utils;
-use stopwatch::{Stopwatch};
+use std::collections::HashSet;
 
 pub fn get_first_double_frequency() -> i32 {
     let mut current_result = 0;
-    let mut results = vec![0];
+    let mut results = HashSet::new();
     let frequencies = utils::frequency_string_to_array(super::data::INPUT).into_iter().cycle();
 
-    let sw = Stopwatch::start_new();
     for freq in frequencies {
         current_result += freq;
         if results.contains(&current_result) {
-            println!("{}", sw.elapsed_ms());
             return current_result;
         } else {
-            results.push(current_result);
+            results.insert(current_result);
         }
     }
     0
