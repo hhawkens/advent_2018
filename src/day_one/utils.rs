5986c41d6c44f5565e68497adcf4734d7f0a65b7
John <purrlinn@web.de>
2018-12-09 23:36:12 +0100

    code cleanup

--- src/day_one/utils.rs
+++ src/day_one/utils.rs
@@ -1,9 +1,9 @@
 pub fn frequency_string_to_array(freq: &str) -> Vec<i32> {
-    let frequency = String::from(freq);
-    let frequency_texts: Vec<&str> = frequency.split('\n').collect();
-    let frequency_nums: Vec<i32> = frequency_texts.into_iter()
-        .map(|f| { f.parse().unwrap() })
+    let frequency_full_text = String::from(freq);
+    let frequencies: Vec<i32> = frequency_full_text
+        .lines()
+        .map(|l| { l.parse().unwrap() })
         .collect();
 
-    frequency_nums
+    frequencies
 }
