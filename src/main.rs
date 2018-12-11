b1aac823fadcfa8522219697ec1810d01828c993
John <purrlinn@web.de>
2018-12-11 23:56:15 +0100

    Changed naming schema

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-mod day_one;
-mod day_two;
+mod day_1;
+mod day_2;
 
 fn main() {
-    let result_1_1 = day_one::result_part_one();
+    let result_1_1 = day_1::result_part_one();
     println!("Result_1_1: {}", result_1_1);
 
-    let result_1_2 = day_one::result_part_two();
+    let result_1_2 = day_1::result_part_two();
     println!("Result_1_2: {}", result_1_2);
 
-    let result_2_1 = day_two::result_part_one();
+    let result_2_1 = day_2::result_part_one();
     println!("Result_2_1: {}", result_2_1);
 
-    let result_2_2 = day_two::result_part_two();
+    let result_2_2 = day_2::result_part_two();
     println!("Result_2_2: {}", result_2_2);
 }
