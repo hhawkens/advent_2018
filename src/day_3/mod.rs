98f2ab96406d2650343322b6f7180a7b5317920a
John <purrlinn@web.de>
2018-12-17 23:54:21 +0100

    Finished 3_2

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -1,9 +1,13 @@
 mod data;
 mod part_one;
-pub mod part_two; // TODO private
-pub mod types; // TODO private
+mod part_two;
+mod types;
 mod utils;
 
 pub fn result_part_one() -> i32 {
     part_one::multiple_claims_count()
+}
+
+pub fn result_part_two() -> i32 {
+    part_two::find_claim_id_without_overlap()
 }
--- src/day_3/part_two.rs
+++ src/day_3/part_two.rs
@@ -1,10 +1,33 @@
 use super::types::*;
 
-pub fn are_claims_overlapping(first: &Rect, second: &Rect) -> bool { // TODO private
-    overlap_horizontally(first, second) && overlap_vertically(first, second)
+pub fn find_claim_id_without_overlap() -> i32 {
+    let claims = super::utils::get_fabric_claims();
+    for (id_outer, claim_outer) in &claims {
+        let mut found_overlap = false;
+        for (id_inner, claim_inner) in &claims {
+            if *id_inner == *id_outer {
+                continue
+            }
+            if are_claims_overlapping(claim_outer, claim_inner) {
+                found_overlap = true;
+                break
+            }
+        }
+
+        if found_overlap {
+            continue
+        } else {
+            return *id_outer;
+        }
+    }
+    -1
 }
 
-fn overlap_horizontally(first: &Rect, second: &Rect) -> bool {
+fn are_claims_overlapping(first: &Rect, second: &Rect) -> bool {
+    are_overlapping_horizontally(first, second) && are_overlapping_vertically(first, second)
+}
+
+fn are_overlapping_horizontally(first: &Rect, second: &Rect) -> bool {
     let (leftmost, rightmost) = get_leftmost_and_rightmost_location(first, second);
     leftmost.location.x + leftmost.size.w > rightmost.location.x
 }
@@ -17,7 +40,7 @@ fn get_leftmost_and_rightmost_location<'a>(first: &'a Rect, second: &'a Rect) ->
     }
 }
 
-fn overlap_vertically(first: &Rect, second: &Rect) -> bool {
+fn are_overlapping_vertically(first: &Rect, second: &Rect) -> bool {
     let (highest, lowest) = get_highest_and_lowest_location(first, second);
     highest.location.y + highest.size.h > lowest.location.y
 }
