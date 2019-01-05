7751ad4d8f402f68e9cea3badcf4be6e1996e29a
John <purrlinn@web.de>
2019-01-05 03:27:48 +0100

    Optimized polymer reaction algorithm
    fix

--- src/day_5/part_one.rs
+++ src/day_5/part_one.rs
@@ -1,38 +1,31 @@
-use num_traits::{Num};
+use num_traits::Num;
 
 pub fn react_polymer(polymer: &str) -> String {
     let mut reacted_polymer = polymer.to_string();
-    let mut indices_to_remove = Vec::new();
     let mut all_reactions_finished = false;
 
     while !all_reactions_finished {
-        let last_index = reacted_polymer.chars().count() - 1;
+        let last_index = reacted_polymer.chars().count();
         let mut skip_one_turn = false;
-        for char_index in 0..last_index {
+        for char_index in (1..last_index).rev() {
             if skip_one_turn {
                 skip_one_turn = false;
                 continue;
             }
 
             let first = reacted_polymer.chars().nth(char_index).unwrap();
-            let second = reacted_polymer.chars().nth(char_index + 1).unwrap();
+            let second = reacted_polymer.chars().nth(char_index - 1).unwrap();
             if do_polymer_parts_react(&first, &second) {
-                indices_to_remove.push(char_index);
-                indices_to_remove.push(char_index + 1);
+                reacted_polymer.remove(char_index);
+                reacted_polymer.remove(char_index - 1);
                 skip_one_turn = true;
             }
         }
 
-        if indices_to_remove.is_empty() {
+        if reacted_polymer.chars().count() == last_index {
             all_reactions_finished = true;
             break;
         }
-
-        indices_to_remove.reverse();
-        for &index_to_remove in &indices_to_remove {
-            reacted_polymer.remove(index_to_remove);
-        }
-        indices_to_remove.clear();
     }
 
     reacted_polymer
