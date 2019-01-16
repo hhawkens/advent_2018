f97c762e12b6364421b5d883fcbe8368c043c48e
John <purrlinn@web.de>
2019-01-16 21:07:45 +0100

    added distance calculation

--- src/day_6/utils.rs
+++ src/day_6/utils.rs
@@ -1,5 +1,6 @@
 use super::types::Point;
 use super::data::COORDINATES;
+use num_traits::real::Real;
 
 pub fn get_coordinates() -> Vec<Point> {
     let mut coordinates = vec![];
@@ -14,4 +15,8 @@ pub fn get_coordinates() -> Vec<Point> {
         });
 
     coordinates
+}
+
+pub fn manhattan_distance(point_1: &Point, point_2: &Point) -> i32 {
+    (point_1.x - point_2.x).abs() + (point_1.y - point_2.y).abs()
 }
