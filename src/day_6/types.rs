c8e6ccf2aa6d45a8a5de06e1fed913d269f5cedb
John <purrlinn@web.de>
2019-01-19 01:47:35 +0100

    Added point extrema calculation
    cleanup
    
    
    cleanup

--- src/day_6/types.rs
+++ src/day_6/types.rs
@@ -1,8 +1,12 @@
 /// Point in a grid coordinate system
-#[derive(Debug, Hash)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 pub struct Point {
     /// To the right
     pub x: i32,
     /// Downwards
     pub y: i32
+}
+
+pub enum PointAxis {
+    X, Y
 }
--- src/day_6/utils.rs
+++ src/day_6/utils.rs
@@ -1,12 +1,12 @@
-use super::types::Point;
-use super::data::COORDINATES;
+use super::types::{Point, PointAxis};
 use num_traits::real::Real;
 use std::collections::HashSet;
+use itertools::Itertools;
 
-pub fn get_coordinates() -> HashSet<Point> {
+pub fn get_coordinates(coord_text: &str) -> HashSet<Point> {
     let mut coordinates = HashSet::new();
 
-    COORDINATES
+    coord_text
         .lines()
         .for_each(|line| {
             let mut points = line.split(", ").map(|p| {
@@ -20,4 +20,40 @@ pub fn get_coordinates() -> HashSet<Point> {
 
 pub fn manhattan_distance(point_1: &Point, point_2: &Point) -> i32 {
     (point_1.x - point_2.x).abs() + (point_1.y - point_2.y).abs()
+}
+
+pub fn get_extreme_points<'a>(points: impl IntoIterator<Item=&'a Point>) -> HashSet<&'a Point> {
+    let mut left_extremes = (i32::max_value(), vec![]);
+    let mut right_extremes = (i32::min_value(), vec![]);
+    let mut up_extremes = (i32::max_value(), vec![]);
+    let mut down_extremes = (i32::min_value(), vec![]);
+    let smaller_than = |i1: i32, i2: i32| i1 < i2;
+    let bigger_than = |i1: i32, i2: i32| i1 > i2;
+
+    for point in points {
+        add_point_if_extreme(point, &mut left_extremes, PointAxis::X, smaller_than);
+        add_point_if_extreme(point, &mut right_extremes, PointAxis::X, bigger_than);
+        add_point_if_extreme(point, &mut up_extremes, PointAxis::Y, smaller_than);
+        add_point_if_extreme(point, &mut down_extremes, PointAxis::Y, bigger_than);
+    }
+
+    left_extremes.1.iter()
+        .chain(&right_extremes.1)
+        .chain(&up_extremes.1)
+        .chain(&down_extremes.1)
+        .dedup()
+        .map(|p| *p)
+        .collect()
+}
+
+fn add_point_if_extreme<'a>(point: &'a Point, (curr_extreme, extreme_points): &mut (i32, Vec<&'a Point>), axis: PointAxis, compare_fn: impl Fn(i32, i32) -> bool) {
+    let point_extreme = match axis { PointAxis::X => point.x, PointAxis::Y => point.y };
+
+    if point_extreme == *curr_extreme {
+        extreme_points.push(point);
+    } else if compare_fn(point_extreme, *curr_extreme) {
+        *curr_extreme = point_extreme;
+        extreme_points.clear();
+        extreme_points.push(point);
+    }
 }
