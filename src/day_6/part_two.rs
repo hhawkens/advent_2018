720349e954866836c346da51d705ce4760613056
John <purrlinn@web.de>
2019-02-03 23:01:20 +0100

    clippy + fmt

--- src/day_6/part_two.rs
+++ src/day_6/part_two.rs
@@ -1,7 +1,7 @@
 use super::types::*;
 use super::utils::*;
-use std::collections::HashMap;
 use std::cmp;
+use std::collections::HashMap;
 
 pub fn get_area_with_total_distance_less_than(points: &[Point], dist: u64) -> Vec<Point> {
     let mut area = Vec::new();
@@ -31,25 +31,37 @@ pub fn get_area_with_total_distance_less_than(points: &[Point], dist: u64) -> Ve
 }
 
 fn get_total_distance_to_points(point: &Point, points: &[Point]) -> u64 {
-    points
-        .iter()
-        .map(|p| { manhattan_distance(point, p) })
-        .sum()
+    points.iter().map(|p| manhattan_distance(point, p)).sum()
 }
 
 fn calculate_centre_point(points: &[Point]) -> Point {
     let extremes = get_all_direction_extremes(points);
     let x_centre = extremes[&AxisDirection::Left] + extremes[&AxisDirection::Right] / 2;
     let y_centre = extremes[&AxisDirection::Up] + extremes[&AxisDirection::Down] / 2;
-    Point {x: x_centre, y: y_centre}
+    Point {
+        x: x_centre,
+        y: y_centre,
+    }
 }
 
 fn get_all_direction_extremes(points: &[Point]) -> HashMap<AxisDirection, i32> {
     let mut extremes = HashMap::new();
-    extremes.insert(AxisDirection::Left, get_extreme_for_direction(points, AxisDirection::Left));
-    extremes.insert(AxisDirection::Right, get_extreme_for_direction(points, AxisDirection::Right));
-    extremes.insert(AxisDirection::Up, get_extreme_for_direction(points, AxisDirection::Up));
-    extremes.insert(AxisDirection::Down, get_extreme_for_direction(points, AxisDirection::Down));
+    extremes.insert(
+        AxisDirection::Left,
+        get_extreme_for_direction(points, AxisDirection::Left),
+    );
+    extremes.insert(
+        AxisDirection::Right,
+        get_extreme_for_direction(points, AxisDirection::Right),
+    );
+    extremes.insert(
+        AxisDirection::Up,
+        get_extreme_for_direction(points, AxisDirection::Up),
+    );
+    extremes.insert(
+        AxisDirection::Down,
+        get_extreme_for_direction(points, AxisDirection::Down),
+    );
 
     extremes
 }
@@ -58,7 +70,7 @@ fn get_extreme_for_direction(points: &[Point], direction: AxisDirection) -> i32 
     let mut maybe_extreme: Option<i32> = None;
 
     for point in points {
-        let new_extreme = get_extreme(point, direction, &maybe_extreme);
+        let new_extreme = get_extreme(point, direction, maybe_extreme);
         match maybe_extreme {
             None => maybe_extreme = Some(new_extreme),
             Some(extreme) => {
@@ -72,31 +84,23 @@ fn get_extreme_for_direction(points: &[Point], direction: AxisDirection) -> i32 
     maybe_extreme.unwrap_or(0)
 }
 
-fn get_extreme(point: &Point, direction: AxisDirection, comp_val: &Option<i32>) -> i32 {
+fn get_extreme(point: &Point, direction: AxisDirection, comp_val: Option<i32>) -> i32 {
     match direction {
-        AxisDirection::Left => {
-            match comp_val {
-                None => return point.x,
-                Some(x) => return cmp::min(*x, point.x),
-            }
+        AxisDirection::Left => match comp_val {
+            None => point.x,
+            Some(x) => cmp::min(x, point.x),
         },
-        AxisDirection::Right => {
-            match comp_val {
-                None => return point.x,
-                Some(x) => return cmp::max(*x, point.x),
-            }
+        AxisDirection::Right => match comp_val {
+            None => point.x,
+            Some(x) => cmp::max(x, point.x),
         },
-        AxisDirection::Up => {
-            match comp_val {
-                None => return point.y,
-                Some(y) => return cmp::min(*y, point.y),
-            }
+        AxisDirection::Up => match comp_val {
+            None => point.y,
+            Some(y) => cmp::min(y, point.y),
         },
-        AxisDirection::Down => {
-            match comp_val {
-                None => return point.y,
-                Some(y) => return cmp::max(*y, point.y),
-            }
+        AxisDirection::Down => match comp_val {
+            None => point.y,
+            Some(y) => cmp::max(y, point.y),
         },
     }
 }
