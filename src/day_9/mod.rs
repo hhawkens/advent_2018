9053294a1500322c0f05a83bb9a57f88fd19565c
John <purrlinn@web.de>
2019-03-01 23:37:34 +0100

    Finished 9_1

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -1,13 +1,10 @@
-mod types;
+use crate::day_9::utils::play_marble_game;
+
 mod data;
 mod utils;
 
-use self::types::*;
-
-pub fn result_part_one() -> Score {
-    let rules = GameRules{num_players: 10, last_marble_score: 1618};
-    let mut game = Game::new(rules);
-    let winner = game.play();
-    dbg!(&winner);
-    0
+pub fn result_part_one() -> usize {
+    let (num_players, num_marbles) = utils::get_game_rules(data::GAME_RULES);
+    let high_score = play_marble_game(num_players, num_marbles);
+    high_score
 }
--- src/day_9/utils.rs
+++ src/day_9/utils.rs
@@ -1,26 +1,58 @@
-use super::types::*;
+use std::collections::VecDeque;
 
 /// Parses the game rules text to internal data representation
-pub fn get_game_rules(rules_text: &str) -> GameRules {
+pub fn get_game_rules(rules_text: &str) -> (usize, usize) {
     let regex = regex::Regex::new(r"([0-9])\w+").unwrap();
     let mut matches = regex.find_iter(rules_text);
     let num_players = matches.next().unwrap().as_str().parse().unwrap();
-    let last_marble_points = matches.next().unwrap().as_str().parse().unwrap();
-    GameRules {
-        num_players,
-        num_marbles: last_marble_points
-    }
+    let num_marbles = matches.next().unwrap().as_str().parse().unwrap();
+    (num_players, num_marbles)
 }
 
-/// If an index is out of bounds for given collection, this function returns a valid index, as if the
-/// collection was circular
-pub fn circular_index(index: isize, sequence: impl IntoIterator) -> usize {
-    let size = sequence.into_iter().count() as isize;
-    let rem = (index % size).abs();
+pub fn play_marble_game(num_players: usize, num_marbles: usize) -> usize {
+    let mut circle = VecDeque::with_capacity(num_marbles);
+    circle.push_front(0);
+    let mut scores = vec![0_usize; num_players];
 
-    if index >= 0 || rem == 0 {
-        rem as usize
+    for marble in 1..num_marbles {
+        if marble % 23 == 0 {
+            rotate(&mut circle, 7);
+            let score_m = marble;
+            let score_p = circle.pop_front().unwrap();
+            let score = score_m + score_p;
+            scores[marble % num_players] += score;
+        } else {
+            rotate(&mut circle, -2);
+            circle.push_front(marble);
+        }
+    }
+
+    *scores.iter().max().unwrap()
+}
+
+/// Rotates given deque to the right(positive n) or to the left(negative n), emulating a circular collection
+/// (This function was not available in stable std at the time of writing)
+fn rotate<T>(deq: &mut VecDeque<T>, n: isize) {
+    if deq.len() == 0 {
+        return;
+    }
+
+    let abs = n.abs() as usize % deq.len();
+    if n >= 0 {
+        for _ in 0..abs {
+            let popped = deq.pop_back();
+            match popped {
+                Some(val) => deq.push_front(val),
+                None => {},
+            }
+        }
     } else {
-        (size - rem) as usize
+        for _ in 0..abs {
+            let popped = deq.pop_front();
+            match popped {
+                Some(val) => deq.push_back(val),
+                None => {},
+            }
+        }
     }
 }
