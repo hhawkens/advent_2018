a6e8246b7121af642b15b839f3d1a9acfa45cdaa
John <purrlinn@web.de>
2019-03-11 01:25:39 +0100

    Implemented sky rendering

--- src/day_10/types.rs
+++ src/day_10/types.rs
@@ -1,4 +1,5 @@
 use std::ops::Add;
+use std::fmt::{Debug, Formatter, Error};
 
 pub type Velocity = Point;
 
@@ -14,6 +15,11 @@ pub struct Point {
 pub struct Star {
     pub location: Point,
     pub velocity: Velocity,
+}
+
+#[derive(Eq, PartialEq, Hash)]
+pub struct Sky {
+    pub stars: Vec<Star>
 }
 
 impl Add for &Point {
@@ -33,5 +39,73 @@ impl Star {
     pub fn move_one_step(&mut self) {
         self.location.x += self.velocity.x;
         self.location.y += self.velocity.y;
+    }
+}
+
+impl Debug for Sky {
+    /// Custom debug string
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let (left_extr, right_extr, up_extr, down_extr) = self.get_extremes();
+        let vertical_capacity = (down_extr - up_extr + 1) as usize;
+        let horizontal_capacity = (right_extr - left_extr + 1) as usize;
+
+        let mut canvas = (0..vertical_capacity)
+            .map(|line_number| {
+                let relevant_stars = self.stars.iter()
+                    .filter(|&s| s.location.y == line_number as isize + up_extr)
+                    .collect::<Vec<_>>();
+                let mut line = String::with_capacity(horizontal_capacity + 1); // +1 for the line break
+
+                for i in 0..line.capacity() as isize {
+                    if relevant_stars.iter().find(|&&s| s.location.x == i + left_extr ).is_some() {
+                        line.push_str("#");
+                    } else if i == line.capacity() as isize - 1 {
+                        line.push_str("\n");
+                    } else {
+                        line.push_str(".");
+                    }
+                }
+
+                line
+            })
+            .collect::<String>();
+
+        f.write_str(&canvas)
+    }
+}
+
+impl Sky {
+    /// Get the leftmost, rightmost, uppermost and lowermost point values
+    pub fn get_extremes(&self) -> (isize, isize, isize, isize) {
+        let mut left = std::isize::MAX;
+        let mut right = std::isize::MIN;
+        let mut up = std::isize::MAX;
+        let mut down = std::isize::MIN;
+
+        for star in &self.stars {
+            let x = star.location.x;
+            let y = star.location.y;
+
+            if x < left {
+                left = x;
+            }
+            if x > right {
+                right = x;
+            }
+            if y < up {
+                up = y;
+            }
+            if y > down {
+                down = y;
+            }
+        }
+
+        (left, right, up, down)
+    }
+
+    pub fn move_stars_one_step(&mut self) {
+        for star in &mut self.stars {
+            star.move_one_step();
+        }
     }
 }
