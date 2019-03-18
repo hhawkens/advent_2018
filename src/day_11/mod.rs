be74318941094bddff43c98186909c8b0aabaafd
John <purrlinn@web.de>
2019-03-18 22:57:34 +0100

    clippy + fmt

--- src/day_11/mod.rs
+++ src/day_11/mod.rs
@@ -5,6 +5,6 @@ use self::types::*;
 
 pub fn result_part_one() -> Point {
     let grid = Grid::new(300, 300, data::GRID_SERIAL);
-    let largest_3_x_3_power_level= grid.get_largest_power_level_in_sub_grid(Size { x: 3, y: 3 });
+    let largest_3_x_3_power_level = grid.get_largest_power_level_in_sub_grid(Size { x: 3, y: 3 });
     largest_3_x_3_power_level.0
 }
